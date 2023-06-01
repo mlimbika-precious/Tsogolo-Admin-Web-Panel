a0204b60069cd49abeebdbf42b1bb83af794dd4f
mlimbika-precious <bed-com-25-17@unima.ac.mw>
2023-06-02 00:32:25 +0200

    Admin login page

---
+++ adminHeader.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light px-5" style="background-color: orange;">
+    <button class="openbtn" onclick="openNav()" style="margin-right: auto; margin-left: 0;">
+        <i class="fa fa-bars"></i>
+    </button>
+    <div style="text-align: center; width: 100%; height: 30px;">
+        <h2 style="color: white; font-size: 25px;">CAREER GUIDANCE MOBILE APP</h2>
+    </div>
+
+    <a class="navbar-brand ml-5" href="./index.php"></a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
+
+    <!-- Logout Button -->
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="login.php" style="color: white;">
+                <i class="fa fa-sign-out" style="margin-right: 5px;"></i>
+                Logout
+            </a>
+        </li>
+    </ul>
+</nav>
